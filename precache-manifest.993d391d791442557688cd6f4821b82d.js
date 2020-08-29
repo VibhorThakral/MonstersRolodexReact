4b89b6377ebfa414fad2009882548ef1d1dad61f
VibhorThakral <vibhorwebsolutions@gmail.com>
2020-08-29 21:12:04 +0530

    Updates

---
+++ precache-manifest.993d391d791442557688cd6f4821b82d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7d804427e70a105b415bf12e61f628",
+    "url": "/MonstersRolodexReact/index.html"
+  },
+  {
+    "revision": "897fdeb3a5b29adba24b",
+    "url": "/MonstersRolodexReact/static/css/main.84d2ed50.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/MonstersRolodexReact/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodexReact/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897fdeb3a5b29adba24b",
+    "url": "/MonstersRolodexReact/static/js/main.7c984559.chunk.js"
+  },
+  {
+    "revision": "090e5a91a059821eb890",
+    "url": "/MonstersRolodexReact/static/js/runtime-main.d6524c3f.js"
+  }
+]);
